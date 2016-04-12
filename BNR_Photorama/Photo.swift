8e113724b8f3984bb6110bf37d1f002936bad0e9
Yohannes Wijaya <readyforservice@icloud.com>
2016-04-12 17:32:40 +0700

    19. Web Services: Modeling the Photo

---
+++ BNR_Photorama/Photo.swift
@@ -0,0 +1,28 @@
+//
+//  Photo.swift
+//  BNR_Photorama
+//
+//  Created by Yohannes Wijaya on 4/12/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    // MARK: - Stored Properties
+    
+    let title: String
+    let remoteURL: NSURL
+    let photoID: String
+    let dateTaken: NSDate
+    
+    // MARK: - Designated Initializer
+    
+    init(title: String, remoteURL: NSURL, photoID: String, dateTaken: NSDate) {
+        self.title = title
+        self.remoteURL = remoteURL
+        self.photoID = photoID
+        self.dateTaken = dateTaken
+    }
+}
