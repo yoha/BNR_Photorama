3677338f7b9d4f08368ae55988b55cf8d423f5f3
Yohannes Wijaya <readyforservice@icloud.com>
2016-04-12 11:52:50 +0700

    19: Web Services: Starting the Photorama Application

---
+++ BNR_Photorama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  BNR_Photorama
+//
+//  Created by Yohannes Wijaya on 4/12/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
